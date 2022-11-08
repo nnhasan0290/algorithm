137c785cc89580e2bc04cac5d962f6c00ba4ead2
Nazmul Hasan <nazmulhasan02901@gmail.com>
2022-11-08 11:00:29 +0000

    main

--- alarmClock/alarm.js
+++ alarmClock/alarm.js
@@ -3,9 +3,10 @@ class Alarm {
     this.now = new Date();
     this.year = this.now.getFullYear();
     this.month = this.now.getMonth();
-    // this.day = this.now.getDate();
+     this.day = this.now.getDate();
     this.hour = this.now.getHours();
-    // this.minute = this.now.getMinutes();
+    this.minute = this.now.getMinutes();
+    this.seconds = this.now.getSeconds();
     this.added__alarm = [];
   }
 
@@ -30,19 +31,43 @@ class Alarm {
 
   setAlarm(day, hour, minute, pm = false) {
     const { d, h, m } = this.sanitize(day, hour, minute, pm);
-    this.added__alarm.push({time:this.findTime(d,h,m), snooze: 0});
+    this.added__alarm.push({ time: this.findTime(d, h, m), snooze: 0 });
   }
 }
-
-const alarm = new Alarm();
-
-alarm.setAlarm(8, 12, 20, true);
-alarm.setAlarm(9, 12, 20, true);
-
-console.log(alarm.added__alarm);
-
 
 const hour_selector = document.getElementById("hour");
 const minute_selector = document.getElementById("minute");
 
 let options_hour = [];
+
+//Setting hours
+for (let i = 11; i >= 1; i--) {
+  hour_selector.innerHTML += `<option>${i}</option>`;
+}
+
+//setting minutes
+
+for (let i = 01; i <= 59; i++) {
+  minute_selector.innerHTML += `<option>${i}</option>`;
+}
+
+//keep the time updating
+const keepUpdatingTime = () => {
+  const alarm = new Alarm();
+
+  const am_or_pm = alarm.hour > 11 && alarm.hour <24 ? "PM" : "AM" 
+  const hour = alarm.hour > 12 ? alarm.hour - 12 : alarm.hour ;
+
+  document.querySelector(
+    ".current_time"
+  ).innerHTML = `${hour} : ${alarm.minute} : ${alarm.seconds} ${am_or_pm}`;
+
+  document.querySelector(".current_date").innerHTML = `${alarm.day} / ${alarm.month} / ${alarm.year}`
+};
+
+
+
+
+setInterval(() => {
+  keepUpdatingTime();
+}, 1000);
